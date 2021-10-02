8f368c41784400c9c3fd9c001797dd21f570e2cc
fix conflict <andrewburd@iMac-Andrej.local>
2021-10-02 23:28:19 +0300

    create: class popup & popup button & now playing parser

--- script.js
+++ script.js
@@ -1,43 +1,146 @@
+// CLASSES
+
+class Parser {
+  getNowPlayingVideo() {
+    let name = document.querySelector('h1.title').textContent,
+        id = document.querySelector('ytd-watch-flexy[video-id]').getAttribute('video-id');
+
+    return {
+      'sectionName': 'Now Playing:',
+      'items': [
+        {
+          'name': name,
+          'id': id,
+        },
+      ],
+    }
+  }
+
+  getPlaylistVideo() {}
+}
+
 class Popup {
-  constructor() {
-    this.template = `
-    <div class="get-play-popup">
-      <span class="get-play-popup__button">GetLinks</span>
-      <textarea class="get-play-popup__result"></textarea>
-    </div>
-    `;
+  constructor(elements) {
+    this.elements = elements;
+    this.status = false;
   }
 
   init() {
     let popupBody = document.createElement('div');
 
-    popupBody.classList.add('get-play-popup__container');
-    popupBody.innerHTML = this.template;
+    popupBody.classList.add('get-play__popup');
     document.body.prepend(popupBody);
-    console.log('initial!');
 
-    let button = popupBody.querySelector('.get-play-popup__button');
+    for(let key in this.elements) {
+      let part = this.elements[key],
+          title = document.createElement('h2'),
+          items = document.createElement('div')
 
-    button.addEventListener('click', this.clickOn );
+      title.classList.add('get-play__popup-title');
+      title.innerText = part.sectionName;
+
+      for(let key in part.items) {
+        let item = part.items[key],
+            element = `
+            <div class="popup-row__name">${item.name}</div>
+            <div class="popup-row__links">
+              <iframe src="https://www.yt-download.org/api/button/videos/${item.id}" width="50%" height="100px" scrolling="no" style="border:none;"></iframe>
+              <iframe src="https://www.yt-download.org/api/button/mp3/${item.id}" width="50%" height="100px" scrolling="no" style="border:none;"></iframe>
+            </div>
+        `,
+            elementContainer = document.createElement('div');
+
+        elementContainer.classList.add('get-play__popup-row');
+        elementContainer.innerHTML = element;
+        items.append(elementContainer);
+      }
+
+      popupBody.append(title);
+      popupBody.append(items);
+    }
+
+    this.status = true;
   }
 
-  clickOn() {
-    let videoNodes = document.querySelectorAll('ytd-grid-video-renderer'),
-        area = document.body.querySelector('.get-play-popup .get-play-popup__result'),
-        baseLink = 'https://youtube.com';
+  visibility() {
+    let popupBody = document.querySelector('.get-play__popup');
+    if(!this.status) {
+      popupBody.style.visibility = 'visible';
+      popupBody.style.opacity = 1;
+      this.status = true;
+    } else {
+      popupBody.style.opacity = 0;
+      setTimeout(()=>{
+        popupBody.style.visibility = 'hidden';
+        this.status = false;
+      }, 500);
+    }
 
-    console.log();
-
-    videoNodes.forEach(el => {
-      let url = el.querySelector('ytd-thumbnail a#thumbnail').getAttribute('href');
-
-      console.log(baseLink + url);
-    })
   }
 }
 
-console.clear();
 
-let popup = new Popup();
+// FUNCTIONS
 
-popup.init();
+function getStartBlock() {
+  let start = document.getElementById('start');
+  if(!start) {
+    window.setTimeout(getStartBlock, 1000);
+    return;
+  }
+  return start;
+}
+
+function createPopupButton(parrent) {
+  let button = document.createElement('div');
+
+  button.classList.add('get-play__button');
+  parrent.append(button);
+
+  return button;
+}
+
+
+// LOGIC
+
+window.addEventListener('load', () => {
+
+  let start = getStartBlock(),
+      genButt = createPopupButton(start),
+      pars = new Parser,
+      popup = new Popup([pars.getNowPlayingVideo()]);
+
+  popup.init();
+
+
+  genButt.addEventListener('click', ()=>{
+    console.clear();
+    let activeClass = 'get-play__button--active';
+
+    if(genButt.classList.contains(activeClass)) {
+      genButt.classList.remove(activeClass);
+    } else {
+      genButt.classList.add(activeClass);
+    }
+    popup.visibility();
+
+  });
+
+  document.addEventListener('click', (e)=>{
+    if(e.target.closest('.popup-row__name')) {
+      let item = e.target.closest('.get-play__popup-row'),
+          activeClass = 'popup-row__links--active',
+          linksBlock = item.querySelector('.popup-row__links');
+
+      if(linksBlock.classList.contains(activeClass)) {
+        linksBlock.classList.remove(activeClass);
+      } else {
+        linksBlock.classList.add(activeClass);
+      }
+    }
+  });
+
+});
+
+
+
