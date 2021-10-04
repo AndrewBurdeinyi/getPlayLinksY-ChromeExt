ae5d25deabf02a385d06e6c7dbcad1c5e7d9b309
fix conflict <andrewburd@iMac-Andrej.local>
2021-10-04 22:10:10 +0300

    refactoring iframe load & refactoring popupButton create

--- script.js
+++ script.js
@@ -1,6 +1,82 @@
 // CLASSES
 
+class Popup {
+  constructor(elements) {
+    this.elements = elements;
+    this.status = false;
+  }
+
+  init() {
+    let popupBody = document.createElement('div');
+
+    popupBody.classList.add('get-play__popup');
+    document.body.prepend(popupBody);
+
+    for(let key in this.elements) {
+      let part = this.elements[key],
+          title = document.createElement('h2'),
+          items = document.createElement('div')
+
+      title.classList.add('get-play__popup-title');
+      title.innerText = part.sectionName;
+
+      for(let key in part.items) {
+        let item = part.items[key],
+            element = `
+            <div class="popup-row__name">${item.name}</div>
+            <div class="popup-row__links" data-video-id="${item.id}">
+              <div class="preloader"><div>Loading...</div></div>
+            </div>
+        `,
+            elementContainer = document.createElement('div');
+
+        elementContainer.classList.add('get-play__popup-row');
+        elementContainer.innerHTML = element;
+        items.append(elementContainer);
+      }
+
+      popupBody.append(title);
+      popupBody.append(items);
+    }
+
+  }
+
+  visibility() {
+    let popupBody = document.querySelector('.get-play__popup');
+    if(this.status) {
+      popupBody.style.opacity = 0;
+      setTimeout(()=>{
+        popupBody.style.visibility = 'hidden';
+        this.status = false;
+      }, 500);
+    } else {
+      popupBody.style.visibility = 'visible';
+      popupBody.style.opacity = 1;
+      this.status = true;
+    }
+
+  }
+
+  createIframe(item) {
+    let container = item.querySelector('.popup-row__links'),
+        videoID = container.getAttribute('data-video-id'),
+        iframe = document.createElement('iframe');
+
+    iframe.setAttribute('src', `https://www.yt-download.org/api/widget/mp3/${videoID}`);
+    iframe.setAttribute('width', '100%');
+    iframe.setAttribute('height', '100%');
+    iframe.setAttribute('allowTransparency', 'true');
+    iframe.setAttribute('scrolling', 'no');
+    iframe.setAttribute('style', 'border:none;');
+
+    container.append(iframe);
+
+  }
+
+}
+
 class Parser {
+
   getNowPlayingVideo() {
     let name = document.querySelector('h1.title').textContent,
         id = document.querySelector('ytd-watch-flexy[video-id]').getAttribute('video-id');
@@ -37,83 +113,25 @@ class Parser {
       'items': items,
     }
   }
+
 }
 
-class Popup {
-  constructor(elements) {
-    this.elements = elements;
-    this.status = false;
-  }
-
-  init() {
-    let popupBody = document.createElement('div');
-
-    popupBody.classList.add('get-play__popup');
-    document.body.prepend(popupBody);
-
-    for(let key in this.elements) {
-      let part = this.elements[key],
-          title = document.createElement('h2'),
-          items = document.createElement('div')
-
-      title.classList.add('get-play__popup-title');
-      title.innerText = part.sectionName;
-
-      for(let key in part.items) {
-        let item = part.items[key],
-            element = `
-            <div class="popup-row__name">${item.name}</div>
-            <div class="popup-row__links">
-              <iframe src="https://www.yt-download.org/api/button/videos/${item.id}" width="50%" height="100px" scrolling="no" style="border:none;"></iframe>
-              <iframe src="https://www.yt-download.org/api/button/mp3/${item.id}" width="50%" height="100px" scrolling="no" style="border:none;"></iframe>
-            </div>
-        `,
-            elementContainer = document.createElement('div');
-
-        elementContainer.classList.add('get-play__popup-row');
-        elementContainer.innerHTML = element;
-        items.append(elementContainer);
-      }
-
-      popupBody.append(title);
-      popupBody.append(items);
-    }
-  }
-
-  visibility() {
-    let popupBody = document.querySelector('.get-play__popup');
-    if(this.status) {
-      popupBody.style.opacity = 0;
-      setTimeout(()=>{
-        popupBody.style.visibility = 'hidden';
-        this.status = false;
-      }, 500);
-    } else {
-      popupBody.style.visibility = 'visible';
-      popupBody.style.opacity = 1;
-      this.status = true;
-    }
-
-  }
-}
 
 
 // FUNCTIONS
 
-function getStartBlock() {
+function createPopupButton() {
   let start = document.getElementById('start');
+
   if(!start) {
-    window.setTimeout(getStartBlock, 5000);
+    window.setTimeout(createPopupButton, 2000);
     return;
   }
-  return start;
-}
 
-function createPopupButton(parrent) {
   let button = document.createElement('div');
 
   button.classList.add('get-play__button');
-  parrent.append(button);
+  start.append(button);
 
   return button;
 }
@@ -123,8 +141,7 @@ function createPopupButton(parrent) {
 
 window.addEventListener('load', () => {
 
-  let start = getStartBlock(),
-      genButt = createPopupButton(start),
+  let genButt = createPopupButton(),
       pars = new Parser,
       popup = new Popup([pars.getNowPlayingVideo(), pars.getPlaylistVideo()]);
 
@@ -132,7 +149,6 @@ window.addEventListener('load', () => {
 
 
   genButt.addEventListener('click', ()=>{
-    // console.clear();
     let activeClass = 'get-play__button--active';
 
     if(genButt.classList.contains(activeClass)) {
@@ -151,11 +167,16 @@ window.addEventListener('load', () => {
           activeClass = 'popup-row__links--active',
           linksBlock = item.querySelector('.popup-row__links');
 
+      if(!linksBlock.querySelector('iframe')) {
+        popup.createIframe(item);
+      }
+
       if(linksBlock.classList.contains(activeClass)) {
         linksBlock.classList.remove(activeClass);
       } else {
         linksBlock.classList.add(activeClass);
       }
+
     }
   });
 
